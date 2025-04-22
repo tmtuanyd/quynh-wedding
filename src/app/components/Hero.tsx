39c7238db93ed5bdcd87e844dc022f599d3ea835
tmtuanyd <tmtuanyd@gmail.com>
2025-04-23 03:34:15 +0700

    feat: update

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -5,12 +5,12 @@ export default function Hero() {
   return (
     <motion.section
       id="hero"
-      className="h-screen flex items-center justify-center bg-cover  bg-top  text-white bg-[url('/images/hero.jpg')] "
+      className="min-h-screen flex items-center justify-center bg-cover  bg-top  text-white bg-[url('/images/hero.jpg')] "
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
     >
-      <div className="absolute inset-0 bg-black/50"></div>
+      <div className="absolute min-h-screen inset-0 bg-black/50"></div>
       <div className="text-center space-y-4 relative">
         <h1 className="text-4xl md:text-6xl font-great">
           We Are Getting Married!
