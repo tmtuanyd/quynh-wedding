83274fb7a8f3a844cc2918e2219d37b0800f1d60
tmtuanyd <tmtuanyd@gmail.com>
2025-04-23 20:12:25 +0700

    feat: co dau chu re

--- src/app/components/BrideGroom.tsx
+++ src/app/components/BrideGroom.tsx
@@ -1,32 +1,52 @@
+import Image from "next/image";
 import React from "react";
+import { motion } from "framer-motion";
 
 const BrideGroom = () => {
+  const brideTextVariant = {
+    hidden: { opacity: 0, x: 100 }, // Bắt đầu từ bên phải
+    visible: { opacity: 1, x: 0, transition: { duration: 1 } },
+  };
+
+  const groomTextVariant = {
+    hidden: { opacity: 0, x: -100 }, // Bắt đầu từ bên trái
+    visible: { opacity: 1, x: 0, transition: { duration: 1 } },
+  };
   return (
-    <section className="relative bg-white py-16 px-1 text-black">
-      <div className="flex flex-col gap-16 items-center">
+    <section className="relative bg-white  text-black">
+      <div className="flex flex-col gap- items-center">
         {/* Cô Dâu */}
         <div className="relative flex flex-col lg:flex-row gap-4 items-center">
           <div className="w-full lg:w-1/2 flex gap-4 relative items-center">
-            <div className="relative w-[50%] h-[250px] lg:h-[400px] overflow-hidden rounded-tr-[100px]  shadow-lg">
-              <img
+            <div className="relative w-[50%] h-[200px] lg:h-[400px] overflow-hidden rounded-tr-[100px]  shadow-lg">
+              <Image
                 src="/images/gai/co dau.jpg"
                 alt="Cô Dâu"
                 className="object-cover w-full h-full"
+                width={216}
+                height={153}
+                layout="responsive"
               />
             </div>
             <div className="flex absolute left-[45%] top-[50%] gap-1">
-              <div className="w-20 h-28 lg:w-[100px] lg:h-[150px] border border-white overflow-hidden">
-                <img
+              <div className="w-15 h-20 lg:w-[100px] lg:h-[150px] border border-white overflow-hidden">
+                <Image
                   src="/images/gai/co dau1.jpg"
                   alt="Chi tiết cô dâu 1"
                   className="object-cover w-full h-full"
+                  width={216}
+                  height={153}
+                  layout="responsive"
                 />
               </div>
-              <div className="w-20 h-28 w-20 h-28 lg:w-[100px] lg:h-[150px] border border-white overflow-hidden">
-                <img
+              <div className="w-15 h-20 lg:w-[100px] lg:h-[150px] border border-white overflow-hidden">
+                <Image
                   src="/images/gai/co dau1.jpg"
                   alt="Chi tiết cô dâu 2"
                   className="object-cover w-full h-full"
+                  width={216}
+                  height={153}
+                  layout="responsive"
                 />
               </div>
             </div>
@@ -37,39 +57,54 @@ const BrideGroom = () => {
           </div>
 
           <div className="w-full lg:w-1/2">
-            <p className="italic">
+            <motion.p
+              variants={brideTextVariant}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: false, amount: 0.5 }}
+              className="italic"
+            >
               Em - một cô gái đa sầu, đa cảm, thật may mắn khi gặp được anh. Cảm
               ơn anh luôn quan tâm, chăm sóc em thật nhiều, nuông chiều những
               khi em giận hờn vô cớ. Bắt đầu từ hôm nay chúng ta sẽ viết nên một
               chương mới của cuộc đời, bằng tình thương yêu và hạnh phúc. Đồng
               đầy anh nhé!
-            </p>
+            </motion.p>
           </div>
         </div>
 
         {/* Chú Rể */}
         <div className="relative flex flex-col lg:flex-row-reverse gap-4 items-center">
           <div className="w-full lg:w-1/2 flex gap-4 relative flex-row-reverse items-center">
-            <div className="relative w-[50%] h-[250px] lg:h-[400px] overflow-hidden rounded-tl-[100px]  shadow-lg">
-              <img
+            <div className="relative w-[50%] h-[200px] lg:h-[400px] overflow-hidden rounded-tl-[100px]  shadow-lg">
+              <Image
                 src="/images/trai/chu re.jpg"
                 alt="Chú Rể"
                 className="object-cover w-full h-full"
+                width={216}
+                height={153}
+                layout="responsive"
               />
             </div>
             <div className="flex absolute right-[45%] top-[50%] gap-1">
-              <div className="w-20 h-28 lg:w-[100px] lg:h-[150px] border border-white overflow-hidden">
-                <img
+              <div className="w-15 h-20 lg:w-[100px] lg:h-[150px] border border-white overflow-hidden">
+                <Image
                   src="/images/trai/chu re1.jpg"
                   alt="Chi tiết chú rể 1"
                   className="object-cover w-full h-full"
+                  width={216}
+                  height={153}
+                  layout="responsive"
                 />
               </div>
-              <div className="w-20 h-28 w-20 h-28 lg:w-[100px] lg:h-[150px] border border-white overflow-hidden">
-                <img
+              <div className="w-15 h-20 lg:w-[100px] lg:h-[150px] border border-white overflow-hidden">
+                <Image
                   src="/images/trai/chu re1.jpg"
                   alt="Chi tiết chú rể 2"
                   className="object-cover w-full h-full"
+                  width={216}
+                  height={153}
+                  layout="responsive"
                 />
               </div>
             </div>
@@ -80,12 +115,18 @@ const BrideGroom = () => {
           </div>
 
           <div className="w-full lg:w-1/2">
-            <p className="italic">
+            <motion.p
+              variants={groomTextVariant}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: false, amount: 0.5 }}
+              className="italic"
+            >
               Anh - một chàng trai mạnh mẽ, luôn là chỗ dựa vững chắc cho em.
               Cảm ơn em đã đến bên anh, mang đến niềm vui và hạnh phúc. Anh hứa
               sẽ luôn yêu thương, bảo vệ và đồng hành cùng em trên mọi chặng
               đường phía trước.
-            </p>
+            </motion.p>
           </div>
         </div>
       </div>
