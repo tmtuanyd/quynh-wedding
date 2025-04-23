c9767aeeb20b68a79501a13127a050d26524c102
tmtuanyd <tmtuanyd@gmail.com>
2025-04-23 15:01:51 +0700

    feat: update hiệu ứng

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import WaveText from "./WaveText";
+import { motion } from "framer-motion";
 
 export default function About() {
   return (
@@ -16,36 +16,43 @@ export default function About() {
           height={83}
           className="mx-auto filter invert"
         />
-        <p className="text-[30px] md:text-[32px] font-great pt-8">
-          Tới dự lễ thành hôn của hai chúng tôi
-        </p>
-
-        <div className="flex flex-col gap-4">
-          <WaveText
-            text="Trần Thị Mai Huế"
-            className="text-4xl md:text-[52px] font-great yellow-text mx-auto"
-          />
-          <p className="text-3xl md:text-5xl font-clicker">&</p>
-          <WaveText
-            text="Phan Thế Chiến"
-            className="text-4xl md:text-[52px] font-great yellow-text mx-auto"
-          />
-        </div>
-        <div>
-          <div className="md:pt-4">
-            <h3 className="text-2xl md:text-[34px]  uppercase">
-              CHỦ NHẬT <span className="text-[45px] md:text-[90px]">18</span>{" "}
-              tháng 5 2025
-            </h3>
-            <p className="text-base md:text-[24px] mt-4">
-              <span className="mr-2">|</span>08:30-11:00{" "}
-              <span className="mr-2">|</span>Tại nhà gái
+        <motion.div
+          initial={{ opacity: 0, y: 50 }} // Trạng thái ban đầu
+          whileInView={{ opacity: 1, y: 0 }} // Trạng thái khi xuất hiện trong khung nhìn
+          viewport={{ once: false, amount: 0.5 }} // Hiệu ứng chạy lại khi cuộn
+          transition={{ duration: 1 }}
+          className="space-y-8"
+        >
+          <p className="text-[30px] md:text-[32px] font-great pt-8">
+            Tới dự lễ thành hôn của hai chúng tôi
+          </p>
+          <div className="flex flex-col gap-4 ">
+            <p className="text-4xl md:text-[52px] font-great yellow-text mx-auto">
+              Trần Thị Mai Huế
             </p>
-            <p className="text-base md:text-[22px] italic mt-3">
-              Đội 7, thôn Xuân Phú, xã Xuân Phú, thành phố Bắc Giang, Bắc Giang
+            <p className="text-3xl md:text-5xl font-clicker">&</p>
+            <p className="text-4xl md:text-[52px] font-great yellow-text mx-auto">
+              Phan Thế Chiến
             </p>
           </div>
-        </div>
+          <div>
+            <div className="md:pt-4">
+              <h3 className="text-2xl md:text-[34px]  uppercase">
+                CHỦ NHẬT <span className="text-[45px] md:text-[90px]">18</span>{" "}
+                tháng 5 2025
+              </h3>
+              <p className="text-base md:text-[24px] mt-4">
+                <span className="mr-2">|</span>08:30-11:00{" "}
+                <span className="mr-2">|</span>Tại nhà gái
+              </p>
+              <p className="text-base md:text-[22px] italic mt-3">
+                Đội 7, thôn Xuân Phú, xã Xuân Phú, thành phố Bắc Giang, Bắc
+                Giang
+              </p>
+            </div>
+          </div>
+        </motion.div>
+
         <div className="flex justify-center gap-[40px] pt-8">
           <div className="w-[100px] lg:w-[216px]">
             <Image
