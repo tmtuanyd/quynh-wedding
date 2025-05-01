824975019dcd7713a0034d75db3eb8f5124d692a
tmtuanyd <tmtuanyd@gmail.com>
2025-05-01 12:11:11 +0700

    feat: update

--- src/app/dam-ngo/page.tsx
+++ src/app/dam-ngo/page.tsx
@@ -4,9 +4,7 @@ const page = () => {
   return (
     <div className="h-screen w-screen damngo-bg flex relative items-center justify-center flex-col">
       <p className="font-bold  text-[200px] text-red-300">Lễ Dạm Ngõ</p>
-      <p className="text-[150px] font-great mt-[80px]">
-        Mai Huế <span className="text-red-500 mx-[30px]">❤️</span> Thế Chiến
-      </p>
+      <p className="text-[150px] font-great mt-[80px]">Mai Huế & Thế Chiến</p>
       <p className="text-center mt-[50px] text-[50px] absolute bottom-[50px]">
         01/05/2025
       </p>
