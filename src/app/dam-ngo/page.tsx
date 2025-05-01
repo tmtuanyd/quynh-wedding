11673d5352e719bd64b51f0b73deb87811def298
tmtuanyd <tmtuanyd@gmail.com>
2025-05-01 11:44:31 +0700

    dam ngo

---
+++ src/app/dam-ngo/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="h-screen w-screen damngo-bg flex items-center justify-center flex-col">
+      <p className="font-bold  text-[100px] text-red-300">Lễ Dạm Ngõ</p>
+    </div>
+  );
+};
+
+export default page;
