a620cbe1b87caf4532fbcbf1ccee88ad32cf043f
tmtuanyd <tmtuanyd@gmail.com>
2025-05-01 12:11:31 +0700

    feat: update

--- src/app/dam-ngo/page.tsx
+++ src/app/dam-ngo/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const page = () => {
   return (
     <div className="h-screen w-screen damngo-bg flex relative items-center justify-center flex-col">
-      <p className="font-bold  text-[200px] text-red-300">Lễ Dạm Ngõ</p>
+      <p className="font-bold  text-[250px] text-red-300">Lễ Dạm Ngõ</p>
       <p className="text-[150px] font-great mt-[80px]">Mai Huế & Thế Chiến</p>
       <p className="text-center mt-[50px] text-[50px] absolute bottom-[50px]">
         01/05/2025
