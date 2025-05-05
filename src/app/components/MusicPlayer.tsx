631e92a12698ab6ec21cb35d4acc5e67b11d6b2a
tmtuanyd <tmtuanyd@gmail.com>
2025-05-05 21:56:50 +0700

    feat: upodate

--- src/app/components/MusicPlayer.tsx
+++ src/app/components/MusicPlayer.tsx
@@ -1,11 +1,26 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-import { Volume2, VolumeX } from "lucide-react";
+import { GiftIcon, Volume2, VolumeX } from "lucide-react";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    background: "",
+    border: "none",
+  },
+};
 
 export default function BackgroundMusic() {
   const audioRef = useRef<HTMLAudioElement>(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [modalIsOpen, setIsOpen] = useState(false);
 
   const toggleMusic = () => {
     const audio = audioRef.current;
@@ -34,15 +49,47 @@ export default function BackgroundMusic() {
     };
   }, []);
 
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <>
       <audio ref={audioRef} loop src="/music/beautiful-in-white.mp3" />
-      <button
-        onClick={toggleMusic}
-        className="fixed bottom-5 right-5 bg-red-400 hover:bg-red-400/30 backdrop-blur-md p-2 rounded-full text-white z-50"
+      <div className="flex flex-col gap-2 fixed bottom-5 right-5">
+        <button
+          onClick={openModal}
+          className=" bg-red-400 hover:bg-red-400/30 backdrop-blur-md p-2 rounded-full text-white z-50 zoom-animation"
+        >
+          <GiftIcon size={20} />
+        </button>
+        <button
+          onClick={toggleMusic}
+          className=" bg-red-400 hover:bg-red-400/30 backdrop-blur-md p-2 rounded-full text-white z-50"
+        >
+          {isPlaying ? <Volume2 size={20} /> : <VolumeX size={20} />}
+        </button>
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
       >
-        {isPlaying ? <Volume2 size={20} /> : <VolumeX size={20} />}
-      </button>
+        <div className="flex justify-end">
+          <button
+            onClick={toggleMusic}
+            className=" bg-red-400 hover:bg-red-400/30 backdrop-blur-md p-2 w-[40px] h-[40px] rounded-full text-white z-50 ml-auto mb-4"
+          >
+            x
+          </button>
+        </div>
+
+        <img src="/images/qr.JPG" />
+      </Modal>
     </>
   );
 }
